51431bcbeced1cc72421840774d9e011dd6546d7
othaime-en <61498231+othaime-en@users.noreply.github.com>
2025-09-09 12:46:01 +0300

    finalize evalutor function

--- src/evaluator.js
+++ src/evaluator.js
@@ -4,6 +4,7 @@
  */
 
 // Import special forms (will be defined in special-forms.js)
+const specialForms = require("./special-forms");
 
 /**
  * Evaluates an expression in the given scope
@@ -13,8 +14,36 @@
  */
 function evaluate(expr, scope) {
   // Handle literal values (numbers, strings) here
+  if (expr.type == "value") {
+    return expr.value;
+  }
+
   // Handle variable references (words/identifiers) here
+  else if (expr.type == "word") {
+    if (expr.name in scope) {
+      return scope[expr.name];
+    } else {
+      throw new ReferenceError(`Undefined binding: ${expr.name}`);
+    }
+  }
+
   // Handle function applications and special forms here
+  else if (expr.type == "apply") {
+    let { operator, args } = expr;
+
+    // Check if this is a special form (if, while, define, etc.)
+    if (operator.type == "word" && operator.name in specialForms) {
+      return specialForms[operator.name](expr.args, scope);
+    } else {
+      // Regular function call - evaluate operator and arguments
+      let op = evaluate(operator, scope);
+      if (typeof op == "function") {
+        return op(...args.map((arg) => evaluate(arg, scope)));
+      } else {
+        throw new TypeError("Applying a non-function.");
+      }
+    }
+  }
 }
 
 module.exports = { evaluate };
